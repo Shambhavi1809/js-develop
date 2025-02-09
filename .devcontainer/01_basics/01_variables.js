45b916b9616e0fdc41ee46eeadc72b4c2059475c
Shambhavi1809 <44302076+Shambhavi1809@users.noreply.github.com>
2025-02-09 12:22:24 +0000

    add changes regd variables basics

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 12344
+let accName = "SHAM"
+var accEmail = "sham@s.com"
+accCity = "Mumbai"
+
+//accountId = 267524 //not allowed 
+
+console.table([accountId, accName, accEmail, accCity])
+
+//do not use var because of issues in functional and block scope 
