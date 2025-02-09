c1c25101ec44d383b8b7bcd3ffe559c6ed1306a3
Shambhavi1809 <44302076+Shambhavi1809@users.noreply.github.com>
2025-02-09 12:36:51 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict";//use all code as new version of js
+// alert(3+3) //we are using node js and not browser
+
+//int 2to power 3
+//bigint
+//boolean
+//string 
+//null =- standalone value , it is a dataype - special type 
+//undefined value has not been assigned 
+//symbol uniueness identification 
+
+/* above are primitive data types */
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
