1cc51abb6f67ab666685dcf7e00c6810225c2536
Shambhavi1809 <44302076+Shambhavi1809@users.noreply.github.com>
2025-02-09 12:55:50 +0000

    conversions

---
+++ .devcontainer/01_basics/03_conversionOps.js
@@ -0,0 +1,25 @@
+let score = null 
+//console.log(typeof(score))
+//console.log(Number(score))
+
+
+
+//33 => 33
+//""=> 0 "abc"=>NAN
+//TRUE => 1 //FALSE=> 0
+//undefined = 0 
+
+
+let someNumber = 33
+console.log(typeof(someNumber))
+console.log(String(someNumber))
+
+
+
+let isLoggedIn = 1
+console.log(Boolean(isLoggedIn))
+
+//1=> true 0=> false
+//"" => false 
+//"sham" => true 
+
